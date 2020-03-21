371d9217f385d202aa6a65e78801704318a66212
Ibnsan <antvelkol@gmail.com>
2020-03-21 13:02:35 +0200

    Create "hello world"

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="Hello"/>
+    <p> World</p>
   </div>
 </template>
 
